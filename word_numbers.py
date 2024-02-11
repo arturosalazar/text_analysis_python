dd62383a8ca31c8cfe178561fb8b3024b80a3bc7
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-02-11 16:28:41 -0600

    Fix word_numbers to count words and avoid over counting due to extra spaces

--- word_numbers.py
+++ word_numbers.py
@@ -1,17 +1,11 @@
 # Count the number of words in a user's input
 
 def count_words(user_input):
-    counter = 0
-
-    # Only update counter if there is actually input, otherwise input 0 if empty str
-    if user_input:
-        counter += 1
-        for char in user_input:
-            if char == ' ':
-                counter += 1
-
-    return counter
-
+    words = user_input.split(" ")
+    # Remove any empty strings (indicator of extra spaces in input)
+    words = list(filter(None, words))
+    return len(words)
+    
 if __name__ == "__main__":
     user_input = input("Please provide a string: ")
     print(count_words(user_input))
