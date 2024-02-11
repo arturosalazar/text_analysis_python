039e813d4b3503d5be1f7581a6137f5e80469714
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-02-11 16:27:35 -0600

    Add unit tests for word_numbers

--- test_text_analysis.py
+++ test_text_analysis.py
@@ -1,5 +1,6 @@
 import unittest
 from vowel_numbers import count_vowels
+from word_numbers import count_words
 
 class TestVowelNumbers(unittest.TestCase):
     def test_all_vowels(self):
@@ -16,6 +17,22 @@ class TestVowelNumbers(unittest.TestCase):
 
     def test_empty_input(self):
         self.assertEqual(count_vowels(""), 0)
+
+class TestWordNumbers(unittest.TestCase):
+    def test_single_word(self):
+        self.assertEqual(count_words("hello"), 1)
+
+    def test_multiple_words(self):
+        self.assertEqual(count_words("hello there world"), 3)
+    
+    def test_multiple_spaces(self):
+        self.assertEqual(count_words("hello    world"), 2)
+    
+    def test_empty_input(self):
+        self.assertEqual(count_words(""), 0)
+    
+    def test_insert_punctuation(self):
+        self.assertEqual(count_words("Hello, world! "), 2)
 
 if __name__ == "__main__":
     unittest.main()
