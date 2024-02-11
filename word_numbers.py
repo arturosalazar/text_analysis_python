425f42458e66e455f9db2d5075628d4065c218e9
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-02-11 09:38:27 -0600

    Update word_numbers to return value for use with unittest

--- word_numbers.py
+++ word_numbers.py
@@ -1,14 +1,17 @@
 # Count the number of words in a user's input
 
-counter = 0
+def count_words(user_input):
+    counter = 0
 
-user_input = input("Please provide a string: ")
+    # Only update counter if there is actually input, otherwise input 0 if empty str
+    if user_input:
+        counter += 1
+        for char in user_input:
+            if char == ' ':
+                counter += 1
 
-# Only update counter if there is actually input, otherwise input 0 if empty str
-if user_input:
-    counter += 1
-    for char in user_input:
-        if char == ' ':
-            counter += 1
+    return counter
 
-print(counter)
+if __name__ == "__main__":
+    user_input = input("Please provide a string: ")
+    print(count_words(user_input))
