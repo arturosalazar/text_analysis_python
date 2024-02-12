9ed59f3102c79e8bbab72bdd571ef8a3b6f09f54
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-02-11 19:21:47 -0600

    Add format_word_list function to spell_checker

--- spell_checker.py
+++ spell_checker.py
@@ -19,6 +19,22 @@ word_list = set(words.words())
 def is_valid_word_nltk(word):
     return word.lower() in word_list
 
+"""Format words into a list of just strings without punctuation"""
+def format_word_list(input_string):
+
+    # Remove any numbers
+    translation_table_num = str.maketrans('', '', string.digits)
+    text_no_numbers = input_string.translate(translation_table_num)
+    
+    # Remove any punctuation
+    translation_table_punctuation = str.maketrans('', '', string.punctuation)
+    text_no_punctuation = text_no_numbers.translate(translation_table_punctuation)
+    
+    input_words_list = text_no_punctuation.split(" ")
+    input_words_list = list(filter(None, input_words_list))
+    return input_words_list
+
+
 if __name__ == "__main__":
     user_input = input("Please provide a string: ")
-    print(is_valid_word_nltk(user_input))
+    print(format_word_list(user_input))
